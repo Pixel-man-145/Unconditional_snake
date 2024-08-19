bf267d018c65e4f8c73ed7bcaefd82d995470b09
Pixel_man_145 <163060082+Pixel-man-145@users.noreply.github.com>
2024-08-19 20:21:06 +0300

    Base

--- snake.cpp
+++ snake.cpp
@@ -4,31 +4,33 @@
 short invent(short a) {                     //инвентирование
     return (a*-1+1);
 }
-short ifor(short a, short b) {              //или
+short myor(short a, short b) {              //или
     return invent(invent(a)*invent(b));    
 }
 short check_zero(short a) {                 //проверка на ноль
-    short e1 = 0;
-    short e2 = 0;
+    unsigned int e1 = 0;
+    unsigned int e2 = 0;
     e1 = (a*a+1)/1;
     e2 = 1/(a*a+1);
     return e1*e2;
 }
-short equals(short a, short b, short c) {            //проверка
-    short e1 = 0;
-    short e2 = 0;
-    e1 = (a/(b * invent(check_zero(b))  +  1 * check_zero(b))) * invent(ifor(check_zero(a), check_zero(b))) + check_zero(b);
-    e2 = (b/(a * invent(check_zero(a))  +  1 * check_zero(a))) * invent(ifor(check_zero(a), check_zero(b))) + check_zero(a);
-    return e1*e2*invent(opposite(a, b));
+short equals(short a, short b) {            //проверка на равенство
+    unsigned int e1 = 0;
+    unsigned int e2 = 0;
+    e1 = (a+(a*a+b*b+1))/(b+(a*a+b*b+1));
+    e2 = (b+(a*a+b*b+1))/(a+(a*a+b*b+1));
+    return e1*e2;
+}
+short more(short a, short b) {              //проверка на больше
+    unsigned int e1 = 0;
+    e1 = (b+(a*a+b*b+1))/(a+(a*a+b*b+1));
+    return check_zero(e1);
+}
+short less(short a, short b) {              //проверка на меньше
+    unsigned int e1 = 0;
+    e1 = (a+(a*a+b*b+1))/(b+(a*a+b*b+1));
+    return check_zero(e1);
 }
 int main() {
-    std::cout << equals(1, 45646);
-    std::cout << equals(0, 0);
-    std::cout << equals(-1, 0);
-    std::cout << equals(-1, -89);
-    std::cout << equals(0, 1);
-    std::cout << equals(-1 , -1);
-    std::cout << equals(0, -1);
-    std::cout << equals(98, -98);
     return 0;
 }
